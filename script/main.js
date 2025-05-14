bb58cbe3161531811e91af001ebf361d7d9666b3
Jiye Shin <jiye2454@naver.com>
2025-05-14 20:25:17 +0900

    Add:cartPopup

--- script/main.js
+++ script/main.js
@@ -69,20 +69,21 @@
 // 네비게이션
 let mainMenu = $('.headerGnavMainItem');
 let prevSubMenu = $('.headerGnavContents');
+let cartMenu = $('.headerGnavMainItemCart');
 let overlay = $('.gnavOverlay');
 let closeBtn = $('.closeButton');
 
+
 mainMenu.hover(function(){
-    if($(this).is('.headerGnavMainItemCart')==true){
-        //장바구니 팝업창
-    }else{
-        prevSubMenu.css('visibility','hidden');
-        
-        let currentSubMenu = $(this).find('.headerGnavContents');
-        currentSubMenu.css('visibility','visible');
-        $('body').css('overflow', 'hidden');
-        overlay.css('visibility','visible');
+    if($(this).is('.headerGnavMainItemCart')){
+        return;
     }
+    prevSubMenu.css('visibility','hidden');
+    
+    let currentSubMenu = $(this).find('.headerGnavContents');
+    currentSubMenu.css('visibility','visible');
+    $('body').css('overflow', 'hidden');
+    overlay.css('visibility','visible');
 },function(){
     overlay.mouseover(function(){
         prevSubMenu.css('visibility','hidden');
@@ -90,17 +91,29 @@ mainMenu.hover(function(){
         overlay.css('visibility','hidden');
     });
 });
+//네비 클로즈버튼
 closeBtn.on('click',function(){
     prevSubMenu.css('visibility','hidden');
         $('body').css('overflow', 'auto');
     overlay.css('visibility','hidden');
 });
-
+//장바구니 팝업창
+cartMenu.on('mouseenter',function(){
+    $('.cartPopup').css('display', 'block');
+});
+cartMenu.on('mouseleave',function(){
+    $('.cartPopup').css('display','none');
+});
+$('.cartCloseButton').on('click',function(){
+    $('.cartPopup').css('display','none');
+});
 //스크롤 애니메이션
+let scrollDown = false;
 $(window).on('scroll', function(){
     //포인트지점은 뷰포트의 바텀(뷰포트탑+뷰포트전체높이)
     let viewportBottom = $(window).scrollTop() + $(window).height();
     let section = $('.scrollAnimate');
+    scrollDown = true;
     //sectionTop 구하기
     section.each(function(){
         let sectionTop = $(this).offset().top;
@@ -117,7 +130,43 @@ $(window).on('scroll', function(){
             };
         };
     });
+
+    
 });
 // $(window).scrollTop();//뷰포트(브라우저창)의 맨위 위치
 // $(window).height();//뷰포트의 전체높이 == 화면 높이
 // $('섹션').offset().top;//섹션이 scrollTop에서 얼마나 떨어져있는지
+
+//스티키헤더
+let prevScrollTop = 0;
+let headerHeight = $('header').outerHeight();
+setInterval(function(){
+    let currScrollTop = $(window).scrollTop();
+    
+    
+    if(scrollDown){
+        scrollDown = false;
+        console.log(headerHeight);
+        //스크롤 위로 올릴 때
+        if(currScrollTop > prevScrollTop){
+            $('header').animate({top:-headerHeight});
+        }else{//스크롤 아래로 내릴 때
+            if(currScrollTop<=headerHeight+30){
+                //최상단까지 거의 다 왔을때 온전한 헤더 보여주기
+                $('header').css({top:0});
+            }else{
+                //최상단이 아닐때 헤더바텀만 보여주기
+                $('header').animate({top:-50});
+            }
+        };
+        prevScrollTop = currScrollTop;
+    };
+},500);
+
+//백투탑 버튼
+$('.backToTopButton').on('click',function(){
+    window.scrollTo({
+        top : 0,
+        behavior : 'smooth'
+    });
+});
